3a0afeb99c743644abf7c043a672f7c74bf71aad
Hatim Shakir <hatimshakir9179@gmail.com>
2025-02-17 04:21:40 +0530

    chore: [ui] add watermark, socials 🎉

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,39 +1,71 @@
+"use client";
+
+import { ReactNode } from "react";
 import { Button } from "../ui/button";
 import { Stack } from "../ui/stack";
 import Logo from "./Logo";
+import { GithubIcon, LinkedinIcon } from "lucide-react";
+import { Typography } from "../ui/typography";
+import Link from "next/link";
 
 type HeaderAction = {
-  label: string;
+  label: ReactNode;
   onClick: () => void;
 };
 
 type HeaderProps = {
   primaryLabel: string;
-  primaryActions?: HeaderAction[];
 };
 
+const HEADER_ACTIONS: HeaderAction[] = [
+  {
+    label: <GithubIcon className="!size-5 text-blue-800" />,
+    onClick: () => window.open("https://github.com/hatim-s/finflow", "_blank"),
+  },
+  {
+    label: <LinkedinIcon className="!size-5 text-blue-800" />,
+    onClick: () => window.open("https://linkedin.com/in/hatim-s", "_blank"),
+  },
+];
+
 export default function Header(props: HeaderProps) {
-  const { primaryActions } = props;
   return (
     <Stack
-      className="px-8 justify-between py-3 flex-row-reverse"
+      className="px-8 justify-between py-3 flex-row-reverse items-center"
       direction="row"
     >
-      <Stack direction="row">
-        <Logo className="text-blue-800 h-10 w-fit" />
-        {primaryActions?.map((action, index) => {
-          if (index === 0)
-            return (
-              <Button key={index} onClick={action.onClick} variant="default">
-                {action.label}
-              </Button>
-            );
+      <Stack className="items-center" direction="row">
+        {HEADER_ACTIONS.map((action, index) => {
           return (
-            <Button key={index} onClick={action.onClick} variant="secondary">
+            <Button
+              className="hover:bg-blue-100 rounded-full p-2 size-10"
+              key={index}
+              onClick={action.onClick}
+              size="icon"
+              variant="ghost"
+            >
               {action.label}
             </Button>
           );
         })}
+        <Link href="https://github.com/hatim-s/finflow" target="_blank">
+          <Logo className="ml-10 text-blue-800 h-10 w-fit" />
+        </Link>
+      </Stack>
+      <Stack className="items-center gap-x-4" direction="row">
+        <Typography
+          className="text-[14px] font-medium text-blue-800"
+          variant="h4"
+        >
+          Made with ❤️ by{" "}
+          <Link
+            className="hover:underline"
+            href="https://github.com/hatim-s"
+            target="_blank"
+          >
+            @hatim-s
+          </Link>
+        </Typography>
       </Stack>
     </Stack>
   );
